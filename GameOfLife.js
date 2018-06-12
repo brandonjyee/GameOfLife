8da69e7524686c6204fe2d76e70c873b8e280535
Brandon Yee <yee.brandon.j@gmail.com>
2018-06-12 12:46:33 -0500

    base

---
+++ GameOfLife.js
@@ -0,0 +1,207 @@
+class GameOfLife {
+	constructor(width, height, wrap = false) {
+		this.width = width;
+		this.height = height;
+		this.board = this.makeBoard();
+		this.config = {
+			wrap: wrap,
+		}
+	}
+
+	/**
+	 * Returns a 2D Array
+	 */
+
+	makeBoard() {
+		// Create and return an 2D Array
+		// with `this.heigh` as rows and `this.width` as cols.
+		// For example, given a height of 4 and a width of 3, it will generate:
+		// [
+		//  [0, 0, 0],
+		//  [0, 0, 0],
+		//  [0, 0, 0],
+		//  [0, 0, 0],
+		// ]
+		let board = [];
+		// For each row...
+		for (let row = 0; row < this.height; row++) {
+			let rowArr = new Array(this.width);
+			// Initialize each item to 0
+			for (let rowIndex = 0; rowIndex < rowArr.length; rowIndex++) {
+				rowArr[rowIndex] = 0;
+			}
+			board.push(rowArr);
+		}
+		return board;
+	}
+
+	checkInput(row, col) {
+		if (row < 0 || col < 0 || row >= this.height || col >= this.width) {
+			return false;
+		}
+		return true;
+	}
+
+	// row, col are zero-based
+	// Affected by wrap config var
+	getCell(row, col) {
+		// Error checking
+		if (!this.checkInput(row, col)) {
+			return -1;
+		}
+
+		return this.board[row][col];
+	}
+
+	setCell(value, row, col) {
+		if (!this.checkInput(row, col)) {
+			throw Error;
+		}
+
+		this.board[row][col] = value;
+	}
+
+	toggleCell(row, col) {
+		if (!this.checkInput(row, col)) {
+			throw Error;
+		}
+
+		let val = this.board[row][col];
+		this.board[row][col] = val === 0 ? 1 : 0;
+	}
+
+	/**
+	 * Return the amount of living neighbors around a given coordinate.
+	 */
+
+	livingNeighbors(row, col) {
+		let living = 0;
+		for (let i = row - 1; i <= row + 1; i++) {
+			for (let j = col - 1; j <= col + 1; j++) {
+				if (i === row && j === col) {
+					continue;
+				}
+				if (this.getCell(i, j) === 1) living++;
+			}
+		}
+		return living;
+	}
+
+	/**
+	 * Given the present board, apply the rules to generate a new board
+	 */
+
+	tick() {
+		const newBoard = this.makeBoard();
+		// Loop through all the cells
+		// on the existing board and determine, based on it's neighbors,
+		// whether the cell should be dead or alive in the new board
+		// (the next iteration of the game)
+		//
+		// Need to:
+		// 1. Count alive neighbors for all cells
+		// 2. Set the next state of all cells in newBoard,
+		// based on their current alive neighbors
+
+		for (let row = 0; row < this.height; row++) {
+			for (let col = 0; col < this.width; col++) {
+				let currentCellVal = this.getCell(row, col);
+				let livingNbrs = this.livingNeighbors(row, col);
+				//checking current state of cell
+				if (currentCellVal === 1 && (livingNbrs < 2 || livingNbrs > 3)) {
+					//if less than 2 or more than 3 neighbors, cell dies
+					newBoard[row][col] = 0;
+				} else if (currentCellVal === 0 && livingNbrs === 3) {
+					// //cell is dead, if equals to 3 live neighbors, cell lives
+					newBoard[row][col] = 1;
+				} else {
+					newBoard[row][col] = currentCellVal;
+				}
+			}
+		}
+		this.board = newBoard;
+  }
+
+  loadCellFormat(inputStr) {
+    // dots are empty; O's are alive (that's a letter 'O', not zero)
+    // Load at the top left of the board
+    let row = 0;
+    let col = 0;
+    // Process each letter of the inputStr
+    for (let ch of inputStr) {
+      // If the character is a newline, increment row and reset col
+      if (ch === '\n') {
+        row++;
+        col = 0;
+      }
+      // Else, process and load the character into the board
+      else {
+        let val = (ch === '.')? 0 : 1;
+        this.setCell(val, row, col);
+        col++;
+      }
+    }
+  }
+
+  loadRLEFormat(rleStr) {
+    let rle = new RLE(rleStr);
+    console.log(`x: ${rle.x} y: ${rle.y}`);
+    // Process the body
+  }
+}
+
+class RLE {
+  // this: x, y, rule, body
+
+  constructor(rleStr) {
+    /*
+    RLE format: <number times to repeat pattern>
+      b  => dead
+      o  => alive
+      $  => end of line
+    */
+   let lines = asLinesArr(rle);
+   this.x = this.y = null;
+   this.body = '';
+   for (let line of lines) {
+     // Ignore comments
+     if (line && line[0] === '#') {
+       continue;
+     }
+
+     // Parse the width and height
+     if (line && line[0] === 'x') {
+       // Format: x = m, y = n, rule = something
+       let commaSeparatedArr = line.split(',');
+       this.x = commaSeparatedArr[0].trim.split(' ')[2];
+       this.y = commaSeparatedArr[1].trim.split(' ')[2];
+     }
+     // Else it's part of the body
+     else {
+       this.body += line;
+     }
+   }
+  }
+}
+
+function asLinesArr(str) {
+  let lines = [];
+  let line = '';
+
+  // For each character in the str
+  for (let ch of str) {
+    // If there's a newline, reset the line var
+    if (ch === '\n') {
+      if (line) {
+        lines.push(line);
+      }
+      line = '';
+    }
+    // It's not a newline char, add the char to the line var
+    else {
+      line += ch;
+    }
+  }
+  lines.push(line);
+  return lines;
+}
---
+++ main.js
@@ -0,0 +1,165 @@
+const width = 25;
+const height = 20; // width and height dimensions of the board
+
+/**
+ * Create a Game of Life instance
+ */
+
+const gol = new GameOfLife(width, height);
+
+/**
+ * create a table and append to the DOM
+ */
+
+// Actual table cells
+const tds = [];
+
+// <table> element
+const table = document.createElement('tbody');
+// build a table row <tr>
+for (let h = 0; h < height; h++) {
+	const tr = document.createElement('tr');
+	// build a table column <td>
+	for (let w = 0; w < width; w++) {
+		const td = document.createElement('td');
+		// We'll put the coordinates on the cell
+		// Element itself (using dataset),
+		// letting us fetch it in a click listener later.
+		td.dataset.row = h;
+		td.dataset.col = w;
+		tds.push(td);
+		tr.append(td);
+	}
+	table.append(tr);
+}
+document.getElementById('board').append(table);
+
+/**
+ * Draws every cell from the gol instance into an actual, visible DOM element
+ */
+
+const paint = () => {
+	// TODO:
+	//   1. For each <td> in the table:
+	//     a. If its corresponding cell in gol instance is alive,
+	//        give the <td> the `alive` CSS class.
+	//     b. Otherwise, remove the `alive` class.
+	//
+	// To find all the <td>s in the table, you might query the DOM for them, or you
+	// could choose to collect them when we create them in createTable.
+	//
+	// HINT:
+	//   https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+	//   https://developer.mozilla.org/en-US/docs/Web/API/Element/getElementsByTagName
+
+	tds.forEach(function(td) {
+		let row = td.dataset.row;
+		let col = td.dataset.col;
+
+		let currentVal = gol.getCell(row, col);
+		// Add class 'alive' if alive
+		if (currentVal) {
+			td.className = 'alive';
+		} else {
+			td.className = '';
+		}
+	});
+	//[ {td dataset.row = 0; dataset.col = 0 }, {}]
+};
+
+/**
+ * Event Listeners
+ */
+
+document.getElementById('board').addEventListener('mousedown', event => {
+	// TODO: Toggle clicked cell (event.target) and paint
+	console.log('board clicked. tagName: ' + event.target.tagName);
+	if (event.target.tagName !== 'TD') {
+		return;
+	}
+
+	let row = event.target.dataset.row;
+	let col = event.target.dataset.col;
+	gol.toggleCell(row, col);
+	paint();
+});
+
+document.getElementById('step_btn').addEventListener('click', event => {
+	// TODO: Do one gol tick and paint
+	console.log('step btn clicked');
+	gol.tick();
+	paint();
+});
+
+let interval;
+
+document.getElementById('play_btn').addEventListener('click', event => {
+	// TODO: Start playing by calling `tick` and paint
+	// repeatedly every fixed time interval.
+	// HINT:
+	// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+	console.log('play button clicked');
+	// Toggle to be stop button
+	let playBtn = document.getElementById('play_btn');
+	if (playBtn.innerText === 'Play') {
+		playBtn.innerText = 'Stop';
+	} else {
+		playBtn.innerText = 'Play';
+	}
+
+	paint();
+	if (interval) {
+		clearInterval(interval);
+		interval = null;
+		return;
+	}
+	interval = setInterval(function() {
+		gol.tick();
+		paint();
+		console.log('setInterval running');
+	}, 100);
+});
+
+document.getElementById('random_btn').addEventListener('click', event => {
+	// TODO: Randomize the board and paint
+
+	tds.forEach(function(td) {
+		let randoNumber = Math.round(Math.random());
+		let row = td.dataset.row;
+		let col = td.dataset.col;
+
+		console.log(randoNumber);
+		gol.setCell(randoNumber, row, col);
+	});
+	paint();
+	// if (randoNumber === 0) {
+	// 	gol.setCell(0, gol.row, gol.col);
+	// }
+
+	console.log('random btn clicked');
+});
+
+document.getElementById('clear_btn').addEventListener('click', event => {
+	tds.forEach(function(td) {
+		let row = td.dataset.row;
+		let col = td.dataset.col;
+		gol.setCell(0, row, col);
+	});
+	paint();
+	clearInterval(interval);
+	console.log('clear btn clicked');
+});
+
+document.getElementById('file_input').addEventListener('change', () => {
+	let fileList = this.files;
+	console.log('file_input changed. fileList: ' + fileList);
+
+}, false);
+
+
+let acorns = `.O.....
+...O...
+OO..OOO`;
+
+gol.loadCellFormat(acorns);
+paint();
