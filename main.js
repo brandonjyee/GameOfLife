ff8a3af706deca8628fe0297912b282c66e67969
Brandon Yee <yee.brandon.j@gmail.com>
2018-06-13 14:57:52 -0500

    can resize board on the fly now

--- main.js
+++ main.js
@@ -1,43 +1,47 @@
-let width = 25;
-let height = 20; // width and height dimensions of the board
+const DEFAULT_WIDTH = 25;
+const DEFAULT_HEIGHT = 20; // width and height dimensions of the board
 
 /**
- * Create a Game of Life instance
+ * Global state
  */
-
-let gol = new GameOfLife(width, height);
+const state = {
+	gol: new GameOfLife(DEFAULT_WIDTH, DEFAULT_HEIGHT),
+	tds: [],
+};
+// let gol = new GameOfLife(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+setUpBoardUI(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
 /**
  * create a table and append to the DOM
  */
-
-// Actual table cells
-const tds = [];
-
-
-function setUpBoard() {
-
-}
-
-// <table> element
-const table = document.createElement('tbody');
-// build a table row <tr>
-for (let h = 0; h < height; h++) {
-	const tr = document.createElement('tr');
-	// build a table column <td>
-	for (let w = 0; w < width; w++) {
-		const td = document.createElement('td');
-		// We'll put the coordinates on the cell
-		// Element itself (using dataset),
-		// letting us fetch it in a click listener later.
-		td.dataset.row = h;
-		td.dataset.col = w;
-		tds.push(td);
-		tr.append(td);
+function setUpBoardUI(height, width) {
+	// Actual table cells
+	let tds = [];
+	// <table> element
+	const table = document.createElement('tbody');
+	// build a table row <tr>
+	for (let h = 0; h < height; h++) {
+		const tr = document.createElement('tr');
+		// build a table column <td>
+		for (let w = 0; w < width; w++) {
+			const td = document.createElement('td');
+			// We'll put the coordinates on the cell
+			// Element itself (using dataset),
+			// letting us fetch it in a click listener later.
+			td.dataset.row = h;
+			td.dataset.col = w;
+			tds.push(td);
+			tr.append(td);
+		}
+		table.append(tr);
 	}
-	table.append(tr);
+	let boardElem = document.getElementById('board');
+	// Clear if there's an existing board
+	boardElem.innerHTML = '';
+	// Put in the new board
+	boardElem.append(table);
+	state.tds = tds;
 }
-document.getElementById('board').append(table);
 
 /**
  * Draws every cell from the gol instance into an actual, visible DOM element
@@ -51,11 +55,11 @@ const paint = () => {
 	//
 	// To find all the <td>s in the table, might query the DOM for them, or
 	// could choose to collect them when create them in createTable.
-	tds.forEach(function (td) {
+	state.tds.forEach(function (td) {
 		let row = td.dataset.row;
 		let col = td.dataset.col;
 
-		let currentVal = gol.getCell(row, col);
+		let currentVal = state.gol.getCell(row, col);
 		// Add class 'alive' if alive
 		if (currentVal) {
 			td.className = 'alive';
@@ -69,7 +73,7 @@ const paint = () => {
 /**
  * Event Listeners
  */
-
+// Clicking on a board cell
 document.getElementById('board').addEventListener('mousedown', event => {
 	// Toggle clicked cell (event.target) and paint
 	console.log('board clicked. tagName: ' + event.target.tagName);
@@ -79,10 +83,11 @@ document.getElementById('board').addEventListener('mousedown', event => {
 
 	let row = event.target.dataset.row;
 	let col = event.target.dataset.col;
-	gol.toggleCell(row, col);
+	state.gol.toggleCell(row, col);
 	paint();
 });
 
+// Changing the board dimensions
 document.getElementById('boardDim').addEventListener('change', event => {
 	console.log('board dimensions changed');
 	try {
@@ -91,7 +96,10 @@ document.getElementById('boardDim').addEventListener('change', event => {
 		width = inputWidth;
 		height = inputHeight;
 		console.log('width: ' + width + ' height: ' + height);
-		gol = new GameOfLife(width, height);
+		// Initialize a new game engine instance
+		state.gol = new GameOfLife(width, height);
+		// Redraw the board UI
+		setUpBoardUI(height, width);
 		paint();
 	} catch (error) {
 		console.log(error);
@@ -101,7 +109,7 @@ document.getElementById('boardDim').addEventListener('change', event => {
 document.getElementById('step_btn').addEventListener('click', event => {
 	// Do one gol tick and paint
 	console.log('step btn clicked');
-	gol.tick();
+	state.gol.tick();
 	paint();
 });
 
@@ -126,7 +134,7 @@ document.getElementById('play_btn').addEventListener('click', event => {
 		return;
 	}
 	interval = setInterval(function () {
-		gol.tick();
+		state.gol.tick();
 		paint();
 		console.log('setInterval running');
 	}, 100);
@@ -135,23 +143,23 @@ document.getElementById('play_btn').addEventListener('click', event => {
 document.getElementById('random_btn').addEventListener('click', event => {
 	// Randomize the board and paint
 	console.log('random btn clicked');
-	tds.forEach(function (td) {
+	state.tds.forEach(function (td) {
 		let randoNumber = Math.round(Math.random());
 		let row = td.dataset.row;
 		let col = td.dataset.col;
 
 		console.log(randoNumber);
-		gol.setCell(randoNumber, row, col);
+		state.gol.setCell(randoNumber, row, col);
 	});
 	paint();
 });
 
 document.getElementById('clear_btn').addEventListener('click', event => {
 	console.log('clear btn clicked');
-	tds.forEach(function (td) {
+	state.tds.forEach(function (td) {
 		let row = td.dataset.row;
 		let col = td.dataset.col;
-		gol.setCell(0, row, col);
+		state.gol.setCell(0, row, col);
 	});
 	paint();
 	clearInterval(interval);
@@ -168,5 +176,5 @@ let acorns = `.O.....
 ...O...
 OO..OOO`;
 
-gol.loadCellFormat(acorns);
+state.gol.loadCellFormat(acorns);
 paint();
